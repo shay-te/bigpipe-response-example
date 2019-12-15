e21f44610f5f4ad7869cde99554762615bf2e1a3
shay tessler <shay.te@gmail.com>
2019-12-15 11:11:19 +0200

    seperace js/css base path

--- config/config_bigpipe.py
+++ config/config_bigpipe.py
@@ -3,10 +3,11 @@ from django.conf import settings
 
 parent_dir = os.path.abspath(os.pardir)
 
-client_base_path = os.path.join(parent_dir, 'client')
+source_base_path = os.path.join(parent_dir, 'client')
 rendered_output_path = os.path.join(parent_dir, 'public')
 
-CLIENT_BASE_PATH = [client_base_path]
+JS_SOURCE_PATH = [source_base_path]
+CSS_SOURCE_PATH = [source_base_path]
 RENDERED_OUTPUT_PATH = rendered_output_path
 STATIC_URI = settings.STATIC_URL
 IS_PRODUCTION_MODE = not settings.DEBUG
