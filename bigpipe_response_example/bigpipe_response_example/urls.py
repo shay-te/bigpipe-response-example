07246164a804c86d70f6e4e8d8e89e687b261ed8
shay tessler <shay.te@gmail.com>
2019-12-15 10:44:26 +0200

    updater

--- bigpipe_response_example/bigpipe_response_example/urls.py
+++ bigpipe_response_example/bigpipe_response_example/urls.py
@@ -13,15 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from django.views.i18n import JavaScriptCatalog
 
-from bigpipe_response.bigpipe import Bigpipe
 from .views import view_demo
-
-
-Bigpipe.init('../config/config_bigpipe.py')
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
--- bigpipe_response_example/manage.py
+++ bigpipe_response_example/manage.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import signal
 import sys
 
+import hydra
+from omegaconf import OmegaConf
+from hydra.plugins.common.utils import HydraConfig
 
-def main():
+from bigpipe_response.bigpipe import Bigpipe
+from data.app_instance import AppInstance
+
+
+@hydra.main(config_path="../config/app_config.yaml")
+def main(cfg):
+# def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bigpipe_response_example.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,8 +24,33 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+
+    # cfg = OmegaConf.load('../config/app_config.yaml')
+    # OmegaConf.register_resolver('cwd')
+    OmegaConf.register_resolver("_hydra", lambda path:HydraConfig().select("hydra." + path))
+    Bigpipe.init(cfg.bigpipe)  # Setup bigpipe
+    AppInstance.init(cfg.demo)  # Setup app instance
+
+    # execute_from_command_line(setup_django_params(cfg))
     execute_from_command_line(sys.argv)
 
 
+
+def setup_django_params(cfg):
+    cfg.django.params.insert(0, __file__)  # set manage.py
+    cfg.django.params[2] = str(cfg.django.params[2])  # django expect port to be a string
+    return cfg.django.params
+
+
+def handle_kill(signum, frame):
+    print('Signal terminate received will shutdown bigpipe')
+    Bigpipe.get().shutdown()
+    sys.exit(0)
+
+
 if __name__ == '__main__':
+    # hydra.verbose=true
+    signal.signal(signal.SIGTERM, handle_kill)
+    signal.signal(signal.SIGINT, handle_kill)
     main()
